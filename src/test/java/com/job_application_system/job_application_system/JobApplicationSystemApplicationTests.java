ab510984c1f612f19be8185878793d73c29a1820
Alex <tomaalexandra8@gmail.com>
2024-05-30 22:22:50 +0300

    initial commit

---
+++ src/test/java/com/job_application_system/job_application_system/JobApplicationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.job_application_system.job_application_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobApplicationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
